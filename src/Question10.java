7ce19258e0ac30fd8761a6e5fe73421447b71fc6
kvivek0 <vivekksp963@gmail.com]>
2026-06-07 13:33:57 +0530

    added question10

--- src/Question10.java
+++ src/Question10.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+//Write a program to rotate digits of a number towards right by one position.
 
 public class Question10 {
+    public static void main(String []args){
+        int x=34;
+         x=x>>1;
+         System.out.print(x);
+    }
 }
