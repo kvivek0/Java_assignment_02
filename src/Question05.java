f4e5867ee041fcede55676b76d4476ba37a385b4
kvivek0 <vivekksp963@gmail.com]>
2026-06-07 12:53:45 +0530

    added Question 5&6

---
+++ src/Question05.java
@@ -0,0 +1,13 @@
+//  How to identify whether a variable is a value type variable
+//                              or nullable type variables?
+
+//  Value type variable:- those variable which are of primitive type
+//                       ex: int x;
+//                           float y=2.3f;
+//                           double d1;
+
+//  Nullable type variable:- those variable which declared using
+//                             non-primitive type and reference variable.
+//                          ex: consumers c1;
+//                               student s1,s2;
+//                               String []args;
---
+++ src/Question06.java
@@ -0,0 +1,9 @@
+// Why there is an error in the Java statement - float x=3.4; ?
+
+//        "here is the error " :- f is missing
+public class Question06 {
+    public static void main(String []args){
+        float x=3.4f;         //written in two ways ;
+        float y=(float)3.4;
+    }
+}
