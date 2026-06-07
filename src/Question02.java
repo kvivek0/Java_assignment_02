c762ed359925908c79e928d649c1295b34959267
kvivek0 <vivekksp963@gmail.com]>
2026-06-07 12:18:24 +0530

    Added Question no.2

--- src/Question02.java
+++ src/Question02.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+//  Write a program to print a number stored in a
+                     // variable without its last digit.
 
 public class Question02 {
+    public static void main(String []args){
+        int x=243,y;
+          y=x/10;  //coz divide operator creates a point 24.3 but values
+                              // always take integer so 24 would be taken.
+        System.out.println("x without its last digit is "+y);
+    }
+
 }
+
