233abe5fd43c0fec42a45552725cba0672835781
kvivek0 <vivekksp963@gmail.com]>
2026-06-07 12:02:49 +0530

    first commit

---
+++ src/Question01.java
@@ -0,0 +1,11 @@
+//  write a program to find and print unit digit of a
+//                       number stored in a variable?
+
+public class Question01 {
+    public static void main(String []args){
+        int x=4678,y;
+        y=x%10;
+        System.out.println("unit digit of x is "+y);
+
+    }
+}
